70bec5046a468d8eb551186f6c297cecc9ca9db3
Jayapradap M <pradaptamil07@gmail.com>
2026-02-17 03:03:44 +0000

    [jaya] Added uc1: print oops

--- src/OPPSBannerApp.java
+++ src/OPPSBannerApp.java
@@ -1,6 +1,7 @@
 public class OPPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the OPPS Banner App!");
+        // uc1:
+        System.out.println("OOPS");
         
     }
 }
