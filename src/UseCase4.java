f918195324c2c9fd47dac6020306cbe34443ce30
Jayapradap M <pradaptamil07@gmail.com>
2026-03-01 18:25:42 +0000

    [Pradap] Usecase 4 Updated

---
+++ src/UseCase4.java
@@ -0,0 +1,12 @@
+public class UseCase4 { 
+        lines[0]=System.out.println(String.join(" ","   ***       *****  "));
+        lines[1]=System.out.println(String.join(" "," **   **    **    * "));
+        lines[2]=System.out.println(String.join(" "," *     *    **      "));
+        lines[3]=System.out.println(String.join(" "," *     *     ***    "));
+        lines[4]=System.out.println(String.join(" "," *     *        **  "));
+        lines[5]=System.out.println(String.join(" "," **   **   *    **  "));
+        lines[6]=System.out.println(String.join(" ","   ***      *****   "));
+    for (line : lines) {
+        System.out.println(line);
+    }
+}
