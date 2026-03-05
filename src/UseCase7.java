7fbf8aad3697677198a8e657a79348e27783c5c1
Jayapradap M <pradaptamil07@gmail.com>
2026-03-05 07:35:04 +0000

    [pradap]: Usecase7 started

---
+++ src/UseCase7.java
@@ -0,0 +1,19 @@
+import java.*;
+import java.util.Scanner;
+import java.util.StringBuilder;
+public class UseCase7 {
+    static CharacterMapping{
+        private char character;
+        private String pattern;
+        public CharacterMapping(char character, String pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+        public char getCharacter() {
+            return character;
+        }
+        public String getPattern() {
+            return pattern;
+    }
+    
+}
